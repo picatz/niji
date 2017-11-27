b32e331f9315daa66f3d54c1ffa99d7ac8b77d54
Kent Gruber <kgruber1@emich.edu>
2017-11-27 11:53:01 -0500

    update install script formatting
    looked uglyish

--- install.sh
+++ install.sh
@@ -4,27 +4,35 @@
 
 # installation
 function install::niji() {
-	git clone https://github.com/picatz/niji.git # clone repo
-	cd niji																			 # change into repo dir
-	make																				 # build binary
-	make install																 # install binary
-	cd ..																				 # change up one dir
-	rm -rf niji																	 # remove niji repo
+  # clone repo
+  git clone https://github.com/picatz/niji.git 
+  # change into repo dir
+  cd niji																			 
+  # build binary
+  make																				 
+  # install binary
+  make install																 
+  # change up one dir
+  cd ..																				 
+  # remove niji repo
+  rm -rf niji																	 
 }
 
 # final error check
 function check::niji() {
-	if which niji >/dev/null; then
-		echo "Niji has been installed!"
-	else
-		echo "Something went wrong, unable to confirm Ninj installation!"
-	fi
+  if which niji >/dev/null; then
+    echo "Niji has been installed!"
+  else
+    echo "Something went wrong, unable to confirm Ninj installation!"
+  fi
 }
 
 # main function
 function main(){
-	install::niji # install niji
-	check::niji   # check if niji was installed
+  # install niji
+  install::niji 
+  # check if niji was installed
+  check::niji   
 }
 
 # run main function
